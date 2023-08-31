b657b0d94a74e1a0dfa3d459db38667875324409
Joowon-Seo <tjwndnjs1998@gmail.com>
2023-09-01 04:59:34 +0900

    Refactor : MentorRepository 수정 오타 수정

--- src/main/java/com/github/supercodingfinalprojectbackend/repository/MentorRepository.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/repository/MentorRepository.java
@@ -1,7 +1,8 @@
 package com.github.supercodingfinalprojectbackend.repository;
 
+import com.github.supercodingfinalprojectbackend.entity.Mentor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MentorRepository extends JpaRepository<MentorRepository, Long> {
+public interface MentorRepository extends JpaRepository<Mentor, Long> {
 
 }
