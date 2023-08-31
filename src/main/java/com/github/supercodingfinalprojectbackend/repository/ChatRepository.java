5679bda3c8136f588b431631013e60eae3aeafb0
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-01 00:36:30 +0900

    Fix: JPA 리포지토리 빈 생성 에러 해결

--- src/main/java/com/github/supercodingfinalprojectbackend/repository/ChatRepository.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/repository/ChatRepository.java
@@ -2,5 +2,5 @@ package com.github.supercodingfinalprojectbackend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChatRepository extends JpaRepository {
-}
+//public interface ChatRepository extends JpaRepository<> {
+//}
