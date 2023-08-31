2601775a709f83435400d4a67bf100deb571f60c
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-31 16:32:57 +0900

    Feat: 멘토 가상 계좌 엔티티 추가

---
+++ src/main/java/com/github/supercodingfinalprojectbackend/entity/MentorAbstractAccount.java
@@ -0,0 +1,24 @@
+package com.github.supercodingfinalprojectbackend.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id", callSuper = false)
+@Table(name = "mentor_abstract_account")
+public class MentorAbstractAccount extends CommonEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "mentor_abstract_account_id")
+    private int id;
+    @Column(name = "account_number")
+    private String accountNumber;
+    @Column(name = "paymoney")
+    private long paymoney;
+}
