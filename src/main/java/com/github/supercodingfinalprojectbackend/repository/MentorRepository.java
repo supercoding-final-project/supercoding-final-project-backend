0a74fa2de509806b9289a7c59f9dc489f40bffd7
Joowon-Seo <tjwndnjs1998@gmail.com>
2023-09-01 03:39:09 +0900

    Feat : Mentor 관련 Repository 생성 Mentor, MentorCareer, MentorSkillStack Repository 생성

---
+++ src/main/java/com/github/supercodingfinalprojectbackend/repository/MentorRepository.java
@@ -0,0 +1,7 @@
+package com.github.supercodingfinalprojectbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MentorRepository extends JpaRepository<MentorRepository, Long> {
+
+}
