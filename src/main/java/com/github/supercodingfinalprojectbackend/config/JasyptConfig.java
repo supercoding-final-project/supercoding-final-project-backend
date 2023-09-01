eb9c4658bf37053e6a7fe4ed749b2f1da7d592b1
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-01 21:38:24 +0900

    Feat: JasyptConfig 추가

---
+++ src/main/java/com/github/supercodingfinalprojectbackend/config/JasyptConfig.java
@@ -0,0 +1,32 @@
+package com.github.supercodingfinalprojectbackend.config;
+
+import lombok.RequiredArgsConstructor;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.crypto.SecretKey;
+
+@Configuration
+@RequiredArgsConstructor
+public class JasyptConfig {
+    private final SecretKey secretKey;
+
+    @Bean(name = "jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(secretKey.toString());
+        config.setAlgorithm("PBEWithMD5AndDES"); //암호화 알고리즘
+        config.setKeyObtentionIterations("1000"); // 반복할 해싱 회수
+        config.setPoolSize("1"); // 인스턴스 pool
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
