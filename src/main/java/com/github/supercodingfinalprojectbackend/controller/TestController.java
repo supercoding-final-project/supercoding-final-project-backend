3f3a6f3981fcfee2f61237983fecb3462921145d
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-02 21:32:50 +0900

    Refactor: ResponseUtils 테스트 흔적..

--- src/main/java/com/github/supercodingfinalprojectbackend/controller/TestController.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.github.supercodingfinalprojectbackend.controller;
 
 import com.github.supercodingfinalprojectbackend.dto.response.PageResponse;
-import com.github.supercodingfinalprojectbackend.dto.response.JsonResponse;
-import com.github.supercodingfinalprojectbackend.exception.errorcode.ApiErrorCode;
+import com.github.supercodingfinalprojectbackend.dto.response.ResponseUtils;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +18,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/")
     @Operation(summary = "스웨거 정상 동작 테스트")
-    public JsonResponse<PageResponse<String>> test() {
+    public Object test() {
+        // ResponseUtils로 생성된 응답 객체는 모두 ResponseEntity<?>입니다.
 
-        return JsonResponse.<PageResponse<String>>builder()
-                .status(203)
-                .message("hello, world")
-                .data(new PageResponse<String>())
-                .build();
+        PageResponse<String> data = new PageResponse<>();
+
+        // 가장 기본적인 사용법
+        // 상태코드, 메세지, 전달할 데이터를 입력
+        ResponseEntity<?> response1 = ResponseUtils.status(HttpStatus.NO_CONTENT, "응답 메세지", data);
+
+        // 자주 사용되는 상태코드에 대응되는 메서드가 이미 있습니다.
+        ResponseEntity<?> response2 = ResponseUtils.ok("응답 메세지", data);
+        ResponseEntity<?> response3 = ResponseUtils.notFound("응답 메세지", data);
+        ResponseEntity<?> response4 = ResponseUtils.internalServerError("응답 메세지", data);
+
+        // 헤더를 커스텀하고 싶은 경우 빌더 패턴을 사용할 수 있습니다.
+        // 빌더는 status() 메서드의 호출로 생성할 수 있습니다.
+        ResponseEntity<?> response5 = ResponseUtils.status(HttpStatus.NO_CONTENT)
+                .message("빌더 패턴에서 메세지를 작성하지 않으면 빈 문자열이 내려갑니다.")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(data);    // body()를 호출하면 객체가 빌드됩니다. 전달할 데이터가 없다면 build()를 호출해도 됩니다.
+
+        // 빌더 패턴 역시 자주 사용되는 상태 코드에 대응되는 메서드가 이미 있습니다.
+        ResponseEntity<?> response6 = ResponseUtils.created()
+                .message("빌더 패턴에서 메세지를 작성하지 않으면 빈 문자열이 내려갑니다.")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(data);
+
+        return response6;
     }
 }
