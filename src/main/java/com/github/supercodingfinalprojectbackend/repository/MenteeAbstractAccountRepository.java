5c12e837408c07670fa104082897a91a2a312a98
Joowon-Seo <tjwndnjs1998@gmail.com>
2023-09-03 04:29:36 +0900

    Refactor : Repository Annotation 추가

--- src/main/java/com/github/supercodingfinalprojectbackend/repository/MenteeAbstractAccountRepository.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/repository/MenteeAbstractAccountRepository.java
@@ -2,7 +2,9 @@ package com.github.supercodingfinalprojectbackend.repository;
 
 import com.github.supercodingfinalprojectbackend.entity.MenteeAbstractAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MenteeAbstractAccountRepository extends JpaRepository<MenteeAbstractAccount, Long> {
 
 }
--- src/main/java/com/github/supercodingfinalprojectbackend/repository/ReviewRepository.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/repository/ReviewRepository.java
@@ -2,7 +2,9 @@ package com.github.supercodingfinalprojectbackend.repository;
 
 import com.github.supercodingfinalprojectbackend.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
 }
