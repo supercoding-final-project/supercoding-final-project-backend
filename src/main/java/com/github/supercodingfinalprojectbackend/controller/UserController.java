7e1f51b5ab40a465d2d12b3226803d2328b1269c
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-03 18:57:45 +0900

    Feat: 카카오 로그인 인가코드 받는 메서드 작성

--- src/main/java/com/github/supercodingfinalprojectbackend/controller/UserController.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/controller/UserController.java
@@ -1,12 +1,10 @@
 package com.github.supercodingfinalprojectbackend.controller;
 
-import com.github.supercodingfinalprojectbackend.util.ResponseUtils;
-import com.github.supercodingfinalprojectbackend.service.OauthService;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,18 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/user")
 public class UserController {
 
-    private final OauthService oauthService;
-
-    @GetMapping("/login")
-    public ResponseEntity<?> login () {
-        System.out.println("요청 처리함!");
-        oauthService.requestAuthorizationCode();
-
-        return ResponseUtils.ok("이거슨 메세지여", "이거시 데이터여");
-    }
-
-    @GetMapping("/oauth2/kakao/callback")
-    public void acceptKakaoAuthorizationCode(){
-        System.out.println("callback!");
+    @GetMapping("/oauth2/kakao/login")
+    public void kakaoLogin(@RequestParam(name = "code") String code){
+        System.out.println(code);
     }
 }
