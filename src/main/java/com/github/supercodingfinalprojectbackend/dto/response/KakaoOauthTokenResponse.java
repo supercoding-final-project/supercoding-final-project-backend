5ffe9ea39c35d6778eef7c8012cf6babe7d2d268
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-04 09:59:22 +0900

    Refactor: KakaoOauthTokenResponse 필드 순서 변경 및 idToken 필드 추가

--- src/main/java/com/github/supercodingfinalprojectbackend/dto/response/KakaoOauthTokenResponse.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/dto/response/KakaoOauthTokenResponse.java
@@ -7,16 +7,18 @@ import lombok.ToString;
 @Getter
 @ToString
 public class KakaoOauthTokenResponse {
-    @JsonProperty("access_token")
-    private String accessToken;
     @JsonProperty("token_type")
     private String tokenType;
-    @JsonProperty("refresh_token")
-    private String refreshToken;
+    @JsonProperty("access_token")
+    private String accessToken;
+    @JsonProperty("id_token")
+    private String idToken;
     @JsonProperty("expires_in")
     private Integer expiresIn;
-    @JsonProperty("scope")
-    private String scope;
+    @JsonProperty("refresh_token")
+    private String refreshToken;
     @JsonProperty("refresh_token_expires_in")
     private Integer refreshTokenExpiresIn;
+    @JsonProperty("scope")
+    private String scope;
 }
