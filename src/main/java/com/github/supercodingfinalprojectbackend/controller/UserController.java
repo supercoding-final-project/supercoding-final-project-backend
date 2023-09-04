b3d200f91a7bbad977050227be36250dbf09f9e4
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-05 00:16:16 +0900

    Refactor: 비즈니스 로직을 서비스로 위임

--- src/main/java/com/github/supercodingfinalprojectbackend/controller/UserController.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/controller/UserController.java
@@ -1,10 +1,9 @@
 package com.github.supercodingfinalprojectbackend.controller;
 
-import com.github.supercodingfinalprojectbackend.dto.KakaoOauthToken;
-import com.github.supercodingfinalprojectbackend.dto.KakaoUserInfo;
 import com.github.supercodingfinalprojectbackend.service.Oauth2Service;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,8 +17,8 @@ public class UserController {
     private final Oauth2Service oauth2Service;
 
     @GetMapping("/oauth2/kakao/login")
-    public void kakaoLogin(@RequestParam(name = "code") String code){
+    public ResponseEntity<?> kakaoLogin(@RequestParam(name = "code") String code){
         System.out.println(code);
-        oauth2Service.kakaoLogin(code);
+        return oauth2Service.kakaoLogin(code);
     }
 }
