0f81c8f81885fec96d3fbb279dfcdbd8fc065ccf
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-05 11:21:20 +0900

    Feat: 로그인 기록을 DB에 저장하는 LoginRecord 엔티티 추가

---
+++ src/main/java/com/github/supercodingfinalprojectbackend/entity/LoginRecord.java
@@ -0,0 +1,27 @@
+package com.github.supercodingfinalprojectbackend.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@EqualsAndHashCode(of = "loginRecordId", callSuper = false)
+@Table(name = "login_records")
+public class LoginRecord extends CommonEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "login_record_id")
+    private Long loginRecordId;
+    @ManyToOne
+    @Column(name = "user_id")
+    private User user;
+    @Column(name = "role_name")
+    private String roleName;
+    @Column(name = "role_id")
+    private String roleId;
+}
