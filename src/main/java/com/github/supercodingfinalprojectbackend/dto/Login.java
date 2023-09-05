ab33eaa40c7b5ea2d8cb04dc2da268081f47ad07
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-05 12:18:10 +0900

    Feat: Response 내부 클래스 추가

--- src/main/java/com/github/supercodingfinalprojectbackend/dto/Login.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/dto/Login.java
@@ -12,4 +12,23 @@ public class Login {
     private String accessToken;
     private String refreshToken;
     private String roleName;
+
+    @Getter
+    @ToString
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Response {
+        private String userId;
+        private String accessToken;
+        private String refreshToken;
+        private String roleName;
+
+        public Response from(Login login) {
+            String userId = login.getUserId();
+            String accessToken = "Bearer " + login.getAccessToken();
+            String refreshToken = "Bearer " + login.getRefreshToken();
+            String roleName = login.getRoleName();
+            return new Response(userId, accessToken, refreshToken, roleName);
+        }
+    }
 }
