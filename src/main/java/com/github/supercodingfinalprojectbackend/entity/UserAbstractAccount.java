0fd1425840125f0f24efd3fa2097746c1921b92f
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-05 14:43:50 +0900

    Feat: UserAbstractAccount 엔티티 추가

---
+++ src/main/java/com/github/supercodingfinalprojectbackend/entity/UserAbstractAccount.java
@@ -0,0 +1,23 @@
+package com.github.supercodingfinalprojectbackend.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "abstractAccountId", callSuper = false)
+@Table(name = "user_abstract_accounts")
+public class UserAbstractAccount extends CommonEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "abstract_account_id")
+    private Long abstractAccountId;
+    @Column(name = "account_number")
+    private String accountNumber;
+    @Column(name = "paymoney")
+    private Long paymoney;
+}
