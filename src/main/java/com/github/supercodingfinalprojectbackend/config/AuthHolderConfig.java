230c455fc90e2821d7ce9d76ba3f03ddbc85d778
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-05 11:10:59 +0900

    Refactor: ConcurrentMap을 날것으로 쓰기보다 AuthHolder를 사용하도록 수정

--- src/main/java/com/github/supercodingfinalprojectbackend/config/AuthHolderConfig.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/config/AuthHolderConfig.java
@@ -1,16 +1,15 @@
 package com.github.supercodingfinalprojectbackend.config;
 
+import com.github.supercodingfinalprojectbackend.dto.AuthHolder;
 import com.github.supercodingfinalprojectbackend.dto.LoginInfo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.concurrent.ConcurrentHashMap;
 
 @Configuration
 public class AuthHolderConfig {
 
     @Bean("AuthHolder")
-        public ConcurrentHashMap<String, LoginInfo> concurrentHashMap() {
-        return new ConcurrentHashMap<>();
+    public AuthHolder<String, LoginInfo> authHolder() {
+        return new AuthHolder<>();
     }
 }
