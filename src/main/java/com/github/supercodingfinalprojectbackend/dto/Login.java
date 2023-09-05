bdeb5812d5cc867dac2196535138b396e808c973
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-05 16:29:20 +0900

    Refactor: userId 타입을 Long으로 변경

--- src/main/java/com/github/supercodingfinalprojectbackend/dto/Login.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/dto/Login.java
@@ -8,7 +8,7 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class Login {
-    private String userId;
+    private Long userId;
     private String accessToken;
     private String refreshToken;
     private String roleName;
@@ -24,7 +24,7 @@ public class Login {
         private String roleName;
 
         public static Response from(Login login) {
-            Long userId = Long.parseLong(login.getUserId());
+            Long userId = login.getUserId();
             String accessToken = "Bearer " + login.getAccessToken();
             String refreshToken = "Bearer " + login.getRefreshToken();
             String roleName = login.getRoleName();
