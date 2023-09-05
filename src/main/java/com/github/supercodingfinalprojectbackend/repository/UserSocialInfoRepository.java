8539b0d986df1131265fd0e6681b7fabf33af7d1
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-05 14:13:17 +0900

    Feat: 소셜 아이디와 소셜 플랫폼 이름으로 유저 소셜 정보 찾는 쿼리 추가

--- src/main/java/com/github/supercodingfinalprojectbackend/repository/UserSocialInfoRepository.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/repository/UserSocialInfoRepository.java
@@ -4,7 +4,10 @@ import com.github.supercodingfinalprojectbackend.entity.UserSocialInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserSocialInfoRepository extends JpaRepository<UserSocialInfo, Long> {
 
+    Optional<UserSocialInfo> findBySocialIdAndSocialPlatformNameAndIsDeletedIsFalse(Long socialId, String socialPlatformName);
 }
