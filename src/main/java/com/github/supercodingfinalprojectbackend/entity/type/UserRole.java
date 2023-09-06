af5492aee8e2fc193b66a5cf58f8082fea923cb4
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-06 15:53:46 +0900

    Refactor: 불필요한 코드들 제거

--- src/main/java/com/github/supercodingfinalprojectbackend/entity/type/UserRole.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/entity/type/UserRole.java
@@ -1,25 +1,6 @@
 package com.github.supercodingfinalprojectbackend.entity.type;
 
 public enum UserRole {
-    MENTEE("mentee"),
-    MENTOR("mentor");
-    private final String name;
-
-    UserRole(String name) {
-        this.name = name;
-    };
-
-
-
-    public static boolean contains(String roleName) {
-        for (UserRole role : values()) {
-            if (role.name.equals(roleName)) return true;
-        }
-
-        return false;
-    }
-
-
-    @Override
-    public String toString() { return name; }
+    MENTEE,
+    MENTOR;
 }
