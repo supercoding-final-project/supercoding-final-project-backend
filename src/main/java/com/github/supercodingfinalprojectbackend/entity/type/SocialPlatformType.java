45bf3d8dcedf6866dbd267b7621b948a108bb24a
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-06 20:38:46 +0900

    Refactor: Enum으로 변경

--- src/main/java/com/github/supercodingfinalprojectbackend/entity/type/SocialPlatformType.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/entity/type/SocialPlatformType.java
@@ -1,8 +1,6 @@
 package com.github.supercodingfinalprojectbackend.entity.type;
 
-public class SocialPlatformType {
-    public static final String KAKAO = "kakao";
-    public static final String GOOGLE = "google";
-
-    private SocialPlatformType() {};
+public enum SocialPlatformType {
+    KAKAO,
+    GOOGLE;
 }
