f0ca3d1840832be338d67b691206e7f024f7e0eb
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-06 20:27:34 +0900

    Refactor: IsDeleted 체크하는 쿼리 메서드 추가

--- src/main/java/com/github/supercodingfinalprojectbackend/repository/MenteeRepository.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/repository/MenteeRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 @Repository
 public interface MenteeRepository extends JpaRepository<Mentee, Long> {
 
-    Optional<Mentee> findByUser(User user);
+    Optional<Mentee> findByUserAndIsDeletedIsFalse(User user);
 }
