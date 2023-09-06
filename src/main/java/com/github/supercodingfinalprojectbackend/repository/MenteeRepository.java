bbd9d0bd27f5655df873a992c9e6160339f1d59c
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-06 16:31:46 +0900

    Feat: User로 멘티 조회 쿼리 추가

--- src/main/java/com/github/supercodingfinalprojectbackend/repository/MenteeRepository.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/repository/MenteeRepository.java
@@ -1,10 +1,14 @@
 package com.github.supercodingfinalprojectbackend.repository;
 
 import com.github.supercodingfinalprojectbackend.entity.Mentee;
+import com.github.supercodingfinalprojectbackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MenteeRepository extends JpaRepository<Mentee, Long> {
 
+    Optional<Mentee> findByUser(User user);
 }
