ff8b4ed3396ee11e274079c0452bbacf3e042027
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-07 17:46:25 +0900

    Refactor: 충돌 해결

--- src/main/java/com/github/supercodingfinalprojectbackend/repository/MentorRepositoryImpl.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/repository/MentorRepositoryImpl.java
@@ -4,12 +4,18 @@ import static com.github.supercodingfinalprojectbackend.entity.QMentor.mentor;
 import static com.github.supercodingfinalprojectbackend.entity.QMentorSkillStack.mentorSkillStack;
 import static com.github.supercodingfinalprojectbackend.entity.QSkillStack.skillStack;
 
+import com.github.supercodingfinalprojectbackend.dto.MentorDto.MentorInfoResponse;
+import com.github.supercodingfinalprojectbackend.dto.QMentorDto_MentorInfoResponse;
 import com.github.supercodingfinalprojectbackend.entity.Mentor;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -21,25 +27,115 @@ public class MentorRepositoryImpl implements MentorRepositoryCustom {
 
 	@Override
 	public List<Mentor> searchAll(String keyword, List<String> skillStackList){
-		log.info("keyword searchAll : {}", keyword);
 
-		List<Mentor> result = jpaQueryFactory
-				.selectFrom(mentor)
+		return jpaQueryFactory
+				.selectDistinct(mentor)
+				.from(mentor)
 				.leftJoin(mentor.mentorSkillStacks, mentorSkillStack)
 				.leftJoin(mentorSkillStack.skillStack, skillStack)
-				.where(mentorNameLike(keyword))
+				.where(conMentorSkillStack(keyword).or(mentorNameLike(keyword)))
+				.fetch();
+	}
+
+	@Override
+	public List<MentorInfoResponse> searchAllFromDto(String keyword, List<String> skillStacks){
+
+		return jpaQueryFactory
+				.selectDistinct(new QMentorDto_MentorInfoResponse(
+						mentor.mentorId,
+						mentor.user.name,
+						mentor.introduction,
+						mentor.company
+				))
+				.from(mentor)
+				.leftJoin(mentor.mentorSkillStacks, mentorSkillStack)
+				.leftJoin(mentorSkillStack.skillStack, skillStack)
+				.where(conMentorSkillStack(keyword).or(mentorNameLike(keyword)))
+				.fetch();
+	}
+
+	@Override
+	public Page<MentorInfoResponse> searchAllFromDtoWithOffsetPagination(String keyword,
+			List<String> skillStacks, Pageable pageable){
+
+		// Querydsl fetchResults() , fetchCount() Deprecated
+		// 대체 방법으로 해결
+
+		List<MentorInfoResponse> content = jpaQueryFactory
+				.selectDistinct(new QMentor_MentorInfoResponse(
+						mentor.mentorId,
+						mentor.user.name,
+						mentor.introduction,
+						mentor.company
+				))
+				.from(mentor)
+				.leftJoin(mentor.mentorSkillStacks, mentorSkillStack)
+				.leftJoin(mentorSkillStack.skillStack, skillStack)
+				.where(conMentorSkillStack(keyword).or(mentorNameLike(keyword)))
+				.offset(pageable.getOffset())
+				.limit(pageable.getPageSize())
 				.fetch();
 
-		log.info("result : {}", result);
-		return result;
+		JPAQuery<Long> countQuery = jpaQueryFactory
+				.selectDistinct(mentor.count())
+				.from(mentor)
+				.leftJoin(mentor.mentorSkillStacks, mentorSkillStack)
+				.leftJoin(mentorSkillStack.skillStack, skillStack)
+				.where(conMentorSkillStack(keyword).or(mentorNameLike(keyword)));
+
+		return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
 	}
 
-	private BooleanExpression conMentorSkillStack(List<String> skillStacks){
-		return null;
+	@Override
+	public Page<MentorInfoResponse> searchAllFromDtoWithCursorPagination(String keyword,
+			List<String> skillStacks, Long cursorId, Pageable pageable){
+
+		List<MentorInfoResponse> content = jpaQueryFactory
+				.selectDistinct(new QMentorDto_MentorInfoResponse(
+						mentor.mentorId,
+						mentor.user.name,
+						mentor.introduction,
+						mentor.company
+				))
+				.from(mentor)
+				.leftJoin(mentor.mentorSkillStacks, mentorSkillStack)
+				.leftJoin(mentorSkillStack.skillStack, skillStack)
+				.where(
+						getRecordsWithCursorId(cursorId),
+						mentorNameLike(keyword).or(conMentorSkillStack(keyword)),
+						skillStackFiltering(skillStacks)
+				)
+				.limit(pageable.getPageSize())
+				.fetch();
+
+		JPAQuery<Long> countQuery = jpaQueryFactory
+				.selectDistinct(mentor.count())
+				.from(mentor)
+				.leftJoin(mentor.mentorSkillStacks, mentorSkillStack)
+				.leftJoin(mentorSkillStack.skillStack, skillStack)
+				.where(
+						getRecordsWithCursorId(cursorId),
+						mentorNameLike(keyword).or(conMentorSkillStack(keyword)),
+						skillStackFiltering(skillStacks)
+				);
+
+		return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
 	}
 
-	private BooleanExpression mentorNameLike(String keyWord){
-//		return keyWord != null ? mentor.name.like("%" + keyWord + "%") : null;
-		return null;   // 주원님 여기 임시로 넣어놨습니다!
+	private BooleanExpression conMentorSkillStack(String keyword){
+		return keyword != null ? skillStack.skillStackName.like("%" + keyword + "%") : null;
 	}
+
+	private BooleanExpression mentorNameLike(String keyword){
+		return keyword != null ? mentor.user.name.like("%" + keyword + "%") : null;
+	}
+
+	private BooleanExpression skillStackFiltering(List<String> skillStackList){
+		return skillStackList != null ? skillStack.skillStackName.in(skillStackList) : null;
+	}
+
+	private BooleanExpression getRecordsWithCursorId(Long cursorId){
+		return cursorId != null ? mentor.mentorId.gt(cursorId) : null;
+	}
+
 }
