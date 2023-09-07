9fba2dea3c797ec973b33e3b71b245fcfb610b90
hjm12223 <gwa5068@gmail.com>
2023-09-07 15:22:20 +0900

    chore: 프론트와의 연결을 위해 CORS 설정을 하였습니다.

---
+++ src/main/java/com/github/supercodingfinalprojectbackend/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.github.supercodingfinalprojectbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/user"); // 메시지 브로커를 설정 ("/topic"으로 시작하는 주제를 사용)
+        config.setApplicationDestinationPrefixes("/ws"); // 클라이언트가 메시지를 송신할 엔드포인트의 접두사
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/code-velop")// SockJs 연결 주소
+                .setAllowedOriginPatterns("*")
+                .withSockJS();// 낮은 버전에서도 사용 가능헤가 하는 설정
+
+    }
+}
