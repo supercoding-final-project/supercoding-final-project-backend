8555156eddbf324dec7d1f8189a409b6a5ef4323
hjm12223 <gwa5068@gmail.com>
2023-09-07 15:26:39 +0900

    feat: 채팅 DTO 입니다.

---
+++ src/main/java/com/github/supercodingfinalprojectbackend/dto/response/ResponseEnterChatRoom.java
@@ -0,0 +1,18 @@
+package com.github.supercodingfinalprojectbackend.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseEnterChatRoom {
+    private List<ResponseChatLog> chatLog;
+}
