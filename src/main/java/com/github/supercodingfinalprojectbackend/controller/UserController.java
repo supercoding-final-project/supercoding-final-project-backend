cd474189393795bf10725a7a33f17da26019e385
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-07 22:54:26 +0900

    Feat: 멘토 커리어 DTO 추가

--- src/main/java/com/github/supercodingfinalprojectbackend/controller/UserController.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/controller/UserController.java
@@ -1,15 +1,20 @@
 package com.github.supercodingfinalprojectbackend.controller;
 
 import com.github.supercodingfinalprojectbackend.dto.Login;
+import com.github.supercodingfinalprojectbackend.entity.MentorCareer;
 import com.github.supercodingfinalprojectbackend.entity.type.UserRole;
 import com.github.supercodingfinalprojectbackend.exception.errorcode.ApiErrorCode;
 import com.github.supercodingfinalprojectbackend.service.Oauth2Service;
 import com.github.supercodingfinalprojectbackend.util.AuthUtils;
 import com.github.supercodingfinalprojectbackend.util.ResponseUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,7 +24,10 @@ public class UserController {
     private final Oauth2Service oauth2Service;
 
     @GetMapping("/oauth2/kakao/login")
-    public ResponseEntity<ResponseUtils.ApiResponse<Login.Response>> kakaoLogin(@RequestParam(name = "code") String code){
+    @Operation(summary = "카카오 로그인")
+    public ResponseEntity<ResponseUtils.ApiResponse<Login.Response>> kakaoLogin(
+            @RequestParam(name = "code") @Parameter(name = "카카오 인가 코드", required = true) String code
+    ){
         Login login = oauth2Service.kakaoLogin(code);
         Login.Response response = Login.Response.from(login);
         return ResponseUtils.ok("로그인에 성공했습니다.", response);
@@ -34,7 +42,9 @@ public class UserController {
     }
 
     @GetMapping("/switch/{roleName}")
-    public ResponseEntity<ResponseUtils.ApiResponse<Login.Response>> switchRole(@PathVariable String roleName) {
+    @Operation(summary = "역할 전환")
+    public ResponseEntity<ResponseUtils.ApiResponse<Login.Response>> switchRole(
+            @PathVariable(name = "roleName") @Parameter(name = "역할 이름", required = true) String roleName) {
         UserRole userRole;
         try {
             userRole = UserRole.valueOf(roleName.toUpperCase());
@@ -45,5 +55,16 @@ public class UserController {
         Login login = oauth2Service.switchRole(userId, userRole);
         Login.Response response = Login.Response.from(login);
         return ResponseUtils.ok("역할을 성공적으로 전환했습니다.", response);
+    }
+
+    @PostMapping(value = "/role/join/mentor", consumes = {"application/x-www-form-urlencoded; charset=utf-8"})
+    @Operation(summary = "멘토 등록")
+    public void joinMentor(
+            @RequestParam(name = "company") @Parameter(name = "현재 다니는 회사", required = true) String company,
+            @RequestParam(name = "introduction") @Parameter(name = "멘토 소개글", required = true) String introduction,
+            @RequestParam(name = "careers", required = false) @Parameter(name = "커리어") List<MentorCareer> careers,
+            @RequestParam(name = "")
+    ){
+
     }
 }
---
+++ src/main/java/com/github/supercodingfinalprojectbackend/dto/MentorCareerDto.java
@@ -0,0 +1,13 @@
+package com.github.supercodingfinalprojectbackend.dto;
+
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class MentorCareerDto {
+    private String duty;
+    private String period;
+}
