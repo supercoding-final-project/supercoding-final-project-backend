031774b6ee2ad50050a91c03d0b783e84c323f23
jungryuel <zldrktduf0927@naver.com>
2023-09-09 01:10:15 +0900

    Feat: 화상채팅 기능 추가 화상채팅에 필요한 비디오 설정등을 파라미터로 받아와 세션ID를 반환 해주고 반환한 세션ID에 따라 토큰을 생성해 화상채팅 연결을 할 수 있게 만들었습니다 추후 토큰 줄 때 결제일에 맞춰 주는 로직을 작성할 예정입니다

--- src/main/java/com/github/supercodingfinalprojectbackend/exception/errorcode/ApiErrorCode.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/exception/errorcode/ApiErrorCode.java
@@ -6,8 +6,8 @@ import org.springframework.http.HttpStatus;
 public enum ApiErrorCode implements ErrorCode {
     FAIL_TO_SAVE(HttpStatus.INTERNAL_SERVER_ERROR, "서버 측의 문제로 데이터 저장에 실패했습니다."),
     INVALID_PATH_VARIABLE(HttpStatus.BAD_REQUEST, "path variable이 잘못되었습니다."),
-    NOT_AUTHENTICATED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다.")
-    ;
+    NOT_AUTHENTICATED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
+    INVALID_CONNECTION_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 연결 요청입니다");
 
     ApiErrorCode(HttpStatus status, String message) {
         this.message = message;
---
+++ src/main/java/com/github/supercodingfinalprojectbackend/service/VideoService.java
@@ -0,0 +1,58 @@
+package com.github.supercodingfinalprojectbackend.service;
+
+import com.github.supercodingfinalprojectbackend.exception.ApiException;
+import com.github.supercodingfinalprojectbackend.exception.errorcode.ApiErrorCode;
+import io.openvidu.java.client.Connection;
+import io.openvidu.java.client.ConnectionProperties;
+import io.openvidu.java.client.OpenVidu;
+import io.openvidu.java.client.OpenViduHttpException;
+import io.openvidu.java.client.OpenViduJavaClientException;
+import io.openvidu.java.client.Session;
+import io.openvidu.java.client.SessionProperties;
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class VideoService {
+
+    @Value("${openvidu.url}")
+    private String OPENVIDU_URL;
+    @Value("${openvidu.secret}")
+    private String OPENVIDU_SECRET;
+    private OpenVidu openVidu;
+
+    @PostConstruct
+    public void initSession() {
+        this.openVidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
+    }
+
+    //화상연결 버튼 클릭시 해당 세션을 만들어줌
+    //연결 대상과 맞는 세션아이디를 비교해서 줘야하는가? -> 채팅방안에서 화상채팅 연결을 할 것이기 때문에 대상
+    public String createSession(Map<String, Object> params)
+        throws OpenViduJavaClientException, OpenViduHttpException {
+        SessionProperties properties = SessionProperties.fromJson(params).build();
+        Session session = openVidu.createSession(properties);
+        return session.getSessionId();
+    }
+
+    //TODO 당일 연결에 대해  결제 안 한 사람의 경우 추가 예정(결제 내역)
+
+    //연결 할 때 (토큰을 준다)
+    public String connection(String sessionId, Map<String, Object> params)
+        throws OpenViduJavaClientException, OpenViduHttpException {
+        {
+            Session session = openVidu.getActiveSession(sessionId);
+            if (session == null) {
+                throw new ApiException(ApiErrorCode.INVALID_CONNECTION_REQUEST);
+            }
+            ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
+            Connection connection = session.createConnection(properties);
+            return connection.getToken();
+        }
+    }
+    //연결 끊기(세션 Id를 삭제 해야하는가? => 1회성 연결이라 세션이 닫힘 그렇다면 동일한 세션)
+}
