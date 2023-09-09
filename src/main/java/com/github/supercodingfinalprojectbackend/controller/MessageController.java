3edde5e35c42c07ad9cb239a0edac1b4f972624c
hjm12223 <gwa5068@gmail.com>
2023-09-10 00:16:58 +0900

    feat: 채팅 로그 페이지네이션 기능구현

---
+++ src/main/java/com/github/supercodingfinalprojectbackend/controller/MessageController.java
@@ -0,0 +1,37 @@
+package com.github.supercodingfinalprojectbackend.controller;
+
+
+import com.github.supercodingfinalprojectbackend.entity.ChatRoom;
+import com.github.supercodingfinalprojectbackend.exception.errorcode.ApiErrorCode;
+import com.github.supercodingfinalprojectbackend.repository.ChatRoomRepository;
+import com.github.supercodingfinalprojectbackend.service.MessageService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+@RestController
+public class MessageController {
+    private final MessageService messageService;
+    private final ChatRoomRepository chatRoomRepository;
+
+    @GetMapping("/message")
+    public ResponseEntity<?> getMessageByChatRoom(
+            @RequestParam Long ChatRoomId, @RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "20") Integer size){
+
+        ChatRoom chatRoom = chatRoomRepository.findByChatRoomIdAndIsChatIsFalse(ChatRoomId).orElseThrow(ApiErrorCode.ChatRoomId_NOT_FOUND::exception);
+        Pageable pageable = PageRequest.of(page,size, Sort.by("sendAt").descending());
+
+        return messageService.getMessageByChatroom(chatRoom,pageable);
+    }
+}
