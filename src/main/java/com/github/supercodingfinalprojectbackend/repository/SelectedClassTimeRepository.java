19cf7d633d8b1aa3fcacb19cecf34fb591d8c2b6
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-13 14:57:08 +0900

    Fix: 컴파일 에러 해결, 안 쓰이는 코드 제거

--- src/main/java/com/github/supercodingfinalprojectbackend/repository/SelectedClassTimeRepository.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/repository/SelectedClassTimeRepository.java
@@ -12,6 +12,4 @@ import java.util.List;
 public interface SelectedClassTimeRepository extends JpaRepository<SelectedClassTime, Long> {
 
     List<SelectedClassTime> findAllByMentorAndOrderSheetAndIsDeletedIsFalse(Mentor mentor, OrderSheet orderSheet);
-
-    List<SelectedClassTime> findByOrderSheet(List<OrderSheet> orderSheet);
 }
