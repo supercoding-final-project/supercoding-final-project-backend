e3e5eae6f4000d2bad9b48b1266280a4fa817df9
Oh-Myeongjae <omjcws@gmail.com>
2023-09-14 15:45:11 +0900

    Feat: 기술스택 아이콘 이미지 컬럼 추가

--- src/main/java/com/github/supercodingfinalprojectbackend/exception/errorcode/ApiErrorCode.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/exception/errorcode/ApiErrorCode.java
@@ -23,6 +23,7 @@ public enum ApiErrorCode implements ErrorCode {
     NOT_FOUND_MENTEE(HttpStatus.NOT_FOUND, "존재하지 않는 멘티입니다."),
     IS_ALREADY_MENTEE(HttpStatus.OK, "이미 멘티로 로그인 중입니다."),
     CHATROOMID_NOT_FOUND(HttpStatus.NOT_FOUND,"해당 채팅방을 찾을 수 없습니다."),
+    MENTEE_MYPAGE_CHANGEINFO_BAD_REQUEST(HttpStatus.BAD_REQUEST,"이전 닉네임과 중복됩니다."),
     MYPAGE_CHANGEINFO_BAD_REQUEST(HttpStatus.BAD_REQUEST,"이전 닉네임과 중복됩니다."),
 
     IMAGE_EXTENSION_BAD_REQUEST(HttpStatus.BAD_REQUEST, "확장자명을 변경해주십시오."),
