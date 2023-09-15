60e55455c194431d469e267207be5d0ae677e766
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-15 21:38:37 +0900

    Refactor: 카카로 리다이렉션 url 변경, cors 설정 변경

--- src/main/java/com/github/supercodingfinalprojectbackend/security/SecurityConfig.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/security/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
                 .cors().configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
                     config.setAllowedOrigins(List.of("https://super-final-front.vercel.app", "http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:5173", "http://127.0.0.1:5173"));
-                    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
+                    config.setAllowedMethods(List.of("*"));
+                    config.setAllowedHeaders(List.of("*"));
                     return config;
                 })
                 .and()
