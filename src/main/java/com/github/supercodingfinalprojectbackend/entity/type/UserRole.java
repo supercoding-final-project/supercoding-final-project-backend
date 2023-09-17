79bbf81fe4c81e66633b4497a83392e4e9dbbcab
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-17 20:25:19 +0900

    Refactor: UserRole이 CustomEnum을 구현하지 안도록 수정, from 메서드 추가, toString 오버라이딩

--- src/main/java/com/github/supercodingfinalprojectbackend/entity/type/UserRole.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/entity/type/UserRole.java
@@ -1,6 +1,6 @@
 package com.github.supercodingfinalprojectbackend.entity.type;
 
-public enum UserRole implements CustomEnum {
+public enum UserRole {
     NONE,
     MENTEE,
     MENTOR;
@@ -10,6 +10,14 @@ public enum UserRole implements CustomEnum {
     UserRole() { this.redirect = null; };
     UserRole(UserRole redirect) { this.redirect = redirect; }
 
+    private UserRole resolve() { return redirect != null ? redirect.resolve() : this; }
+
+    public UserRole from(String userRoleName) {
+        return valueOf(userRoleName).resolve();
+    }
+
     @Override
-    public UserRole resolve() { return redirect != null ? redirect.resolve() : this; }
+    public String toString() {
+        return resolve().toString();
+    }
 }
