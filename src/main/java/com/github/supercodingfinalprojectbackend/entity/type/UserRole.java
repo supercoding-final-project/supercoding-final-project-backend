2e671aec0b65945ea35eb2f9065c504676ebb643
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-17 20:42:44 +0900

    Refactor: UserRole.parseType() 메서드 수정, getScopeAsStirng() 메서드 추가

--- src/main/java/com/github/supercodingfinalprojectbackend/entity/type/UserRole.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/entity/type/UserRole.java
@@ -1,5 +1,8 @@
 package com.github.supercodingfinalprojectbackend.entity.type;
 
+import java.util.Arrays;
+import java.util.StringJoiner;
+
 public enum UserRole {
     NONE,
     MENTEE,
@@ -12,12 +15,22 @@ public enum UserRole {
 
     private UserRole resolve() { return redirect != null ? redirect.resolve() : this; }
 
-    public UserRole from(String userRoleName) {
-        return valueOf(userRoleName).resolve();
+    public static UserRole parseType(String userRoleName) {
+        try {
+            return valueOf(userRoleName).resolve();
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
     }
 
     @Override
     public String toString() {
         return resolve().toString();
+    }
+
+    public static String getScopeAsString() {
+        StringJoiner sj = new StringJoiner(", ", "[", "]");
+        Arrays.stream(values()).filter(v->v.redirect != null).map(UserRole::toString).forEach(sj::add);
+        return sj.toString();
     }
 }
