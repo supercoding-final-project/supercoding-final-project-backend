bbe3b6404ae4bd435023b5abe972c763601e99d0
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-09-17 13:58:40 +0900

    Feat: UserDto.UserInfoResponse에 of() 메서드 추가

--- src/main/java/com/github/supercodingfinalprojectbackend/dto/UserDto.java
+++ src/main/java/com/github/supercodingfinalprojectbackend/dto/UserDto.java
@@ -1,5 +1,7 @@
 package com.github.supercodingfinalprojectbackend.dto;
 
+import com.github.supercodingfinalprojectbackend.entity.Mentee;
+import com.github.supercodingfinalprojectbackend.entity.Mentor;
 import com.github.supercodingfinalprojectbackend.entity.User;
 import lombok.*;
 
@@ -37,5 +39,17 @@ public class UserDto {
         private String nickname;
         private String thumbnailImageUrl;
         private MentorDto.MentorProfileResponse mentorProfile;
+
+        public static UserInfoResponse of(Mentee mentee, Mentor mentor) {
+            User user = mentee.getUser();
+            return UserInfoResponse.builder()
+                    .userId(user.getUserId())
+                    .menteeId(mentee.getMenteeId())
+                    .email(user.getEmail())
+                    .nickname(user.getNickname())
+                    .thumbnailImageUrl(user.getThumbnailImageUrl())
+                    .mentorProfile(mentor != null ? MentorDto.MentorProfileResponse.from(mentor) : null)
+                    .build();
+        }
     }
 }
